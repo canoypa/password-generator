131144b89504529c7704fec9509fc70ac9eadd5e
Cano <developer.sasazono@gmail.com>
2022-02-27 13:08:52 +0900

    arrayShuffle

---
+++ src/core/array_shuffle.ts
@@ -0,0 +1,8 @@
+export const arrayShuffle = (arr: string[], posRandom: number[]): string[] => {
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const j = Math.floor(posRandom[i] * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+
+  return arr;
+};
