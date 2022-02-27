470cffa2a0a25e47c5e34090c212f20613a394f4
Cano <contact.sasasaky@gmail.com>
2022-02-27 13:07:56 +0900

    def CharType

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -4,3 +4,9 @@ export const uppers = lowers.toUpperCase();
 
 export const similarChars = "lI1O0";
 
+export const CharType = {
+  Digit: "digit",
+  Lower: "lower",
+  Upper: "upper",
+} as const;
+export type CharType = typeof CharType[keyof typeof CharType];
