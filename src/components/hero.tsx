e1f82e1094b56e5e61c510a09af2b9629ae6cea1
Cano <developer.sasazono@gmail.com>
2022-02-28 12:09:43 +0900

    hero comp

---
+++ src/components/hero.tsx
@@ -0,0 +1,17 @@
+import { Stack, Typography } from "@mui/material";
+import { FC } from "react";
+import { Logo } from "./logo";
+
+export const Hero: FC = () => {
+  return (
+    <Stack
+      direction={{ xs: "column", sm: "row" }}
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+    >
+      <Logo sx={{ fontSize: 64 }} />
+      <Typography variant="h5">Password Generator</Typography>
+    </Stack>
+  );
+};
