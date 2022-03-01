57db081e3f71a8fb87436d0db411bc38d367aa79
Cano <developer.sasazono@gmail.com>
2022-03-01 15:42:40 +0900

    settings

---
+++ src/components/settings.tsx
@@ -0,0 +1,57 @@
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  ListSubheader,
+} from "@mui/material";
+import { FC, useState } from "react";
+import { useRecoilState } from "recoil";
+import { IncludeTypes, PasswordLength } from "../core/settings_store";
+import { IncludeTypeDialog } from "./settings/include_type_dialog";
+import { PasswordLengthDialog } from "./settings/password_length_dialog";
+
+export const Settings: FC = () => {
+  const [length, setLength] = useRecoilState(PasswordLength);
+  const [types, setTypes] = useRecoilState(IncludeTypes);
+
+  const [lengthOpen, setLengthOpen] = useState(false);
+  const [typesOpen, setTypesOpen] = useState(false);
+
+  console.log(types);
+
+  return (
+    <>
+      <List>
+        <ListSubheader>Settings</ListSubheader>
+        <ListItem disablePadding>
+          <ListItemButton onClick={() => setLengthOpen(true)}>
+            <ListItemText primary="Password Length" secondary={length} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton onClick={() => setTypesOpen(true)}>
+            <ListItemText
+              primary="Include Characters"
+              secondary={types.join(", ")}
+            />
+          </ListItemButton>
+        </ListItem>
+      </List>
+
+      <PasswordLengthDialog
+        open={lengthOpen}
+        onSubmit={(v) => setLength(v)}
+        onClose={() => setLengthOpen(false)}
+        value={length}
+      />
+
+      <IncludeTypeDialog
+        open={typesOpen}
+        onSubmit={(v) => setTypes(v)}
+        onClose={() => setTypesOpen(false)}
+        value={types}
+      />
+    </>
+  );
+};
