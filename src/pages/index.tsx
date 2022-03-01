365cf1f7034f1bbc8d2e9d9ef7bc061c0f0f8849
Cano <contact.sasasaky@gmail.com>
2022-03-01 15:39:30 +0900

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { Box, Stack } from "@mui/material";
 import type { NextPage } from "next";
+import { Footer } from "../components/footer";
+import { Generate } from "../components/generate";
+import { Hero } from "../components/hero";
+import { Settings } from "../components/settings";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <Stack mx="auto" py={3} maxWidth="sm" spacing={8}>
+      <Hero />
+
+      <Box px={3}>
+        <Generate />
+      </Box>
+
+      <Settings />
+
+      <Footer />
+    </Stack>
+  );
 };
 
 export default Home;
