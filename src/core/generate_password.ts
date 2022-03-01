245e3d8a716838e03e875121c853f18357bb90f1
Cano <developer.sasazono@gmail.com>
2022-03-01 19:00:22 +0900

    rewrite generatePassword

--- src/core/generate_password.ts
+++ src/core/generate_password.ts
@@ -2,8 +2,33 @@ import { arrayShuffle } from "./array_shuffle";
 import { CharType, digits, lowers, uppers } from "./constant";
 import { getRandom } from "./get_random";
 
-const pick = (v: number, chars: string): string => {
-  return chars[Math.round((chars.length - 1) * v)];
+const pick = (rand: number, chars: string): string => {
+  return chars[Math.round((chars.length - 1) * rand)];
+};
+
+const getEnableChars = (excludeChars: string): Record<CharType, string> => {
+  const excludeRegexp = new RegExp(`[${excludeChars}]`, "g");
+
+  const chars: Record<CharType, string> = {
+    [CharType.Digit]: digits.replace(excludeRegexp, ""),
+    [CharType.Lower]: lowers.replace(excludeRegexp, ""),
+    [CharType.Upper]: uppers.replace(excludeRegexp, ""),
+  };
+
+  return chars;
+};
+
+const getAvailableChars = (
+  enableChars: Record<CharType, string>,
+  includeTypes: CharType[]
+): string => {
+  let chars = "";
+
+  for (const type of includeTypes) {
+    chars += enableChars[type];
+  }
+
+  return chars;
 };
 
 export type GeneratePasswordArgs = {
@@ -14,33 +39,53 @@ export type GeneratePasswordArgs = {
 };
 export const generatePassword = (options: GeneratePasswordArgs) => {
   const charRandom = getRandom(options.length);
-  const passwordChars = charRandom.map((v, i) => {
-    if (i === 0) {
-      return pick(v, lowers + uppers);
-    }
+  const posRandom = getRandom(options.length);
 
-    if (i - 1 < options.includeType.length - 1) {
-      const type = options.includeType[i - 1];
+  let passwordChars = [];
+  let shift = 0;
 
-      switch (type) {
-        case CharType.Digit:
-          return pick(v, digits);
-        case CharType.Lower:
-          return pick(v, lowers);
-        case CharType.Upper:
-          return pick(v, uppers);
-        default:
-          throw new Error(`Unknown CharType: ${type}`);
-      }
-    }
+  const enableChars = getEnableChars(options.excludeChars || "");
+  const availableChars = getAvailableChars(enableChars, options.includeType);
 
-    return pick(v, lowers + uppers + digits);
+  const isIncludeLetter =
+    options.includeType.includes(CharType.Lower) ||
+    options.includeType.includes(CharType.Upper);
+
+  if (isIncludeLetter) {
+    const chars = options.includeType
+      .filter((v) => v === CharType.Lower || v === CharType.Upper)
+      .map((v) => enableChars[v])
+      .join("");
+
+    passwordChars[shift] = pick(charRandom[shift], chars);
+
+    shift += 1;
+  }
+
+  if (options.includeType.includes(CharType.Digit)) {
+    passwordChars[shift] = pick(charRandom[shift], enableChars.digit);
+    shift += 1;
+  }
+  if (options.includeType.includes(CharType.Lower)) {
+    passwordChars[shift] = pick(charRandom[shift], enableChars.lower);
+    shift += 1;
+  }
+  if (options.includeType.includes(CharType.Upper)) {
+    passwordChars[shift] = pick(charRandom[shift], enableChars.upper);
+    shift += 1;
+  }
+
+  charRandom.slice(shift).forEach((v) => {
+    passwordChars[shift] = pick(v, availableChars);
+    shift += 1;
   });
 
-  const posRandom = getRandom(options.length - 1);
   const passwordArr = [
-    ...passwordChars.slice(0, 1),
-    ...arrayShuffle(passwordChars.slice(1), posRandom),
+    ...(isIncludeLetter ? passwordChars.slice(0, 1) : []),
+    ...arrayShuffle(
+      isIncludeLetter ? passwordChars.slice(1) : passwordChars,
+      posRandom
+    ),
   ];
 
   const password = passwordArr.join("");
