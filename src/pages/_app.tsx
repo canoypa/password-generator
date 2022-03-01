3b64dd039bfdd936f68d3afa019a8bbf6a4b70f4
Cano <developer.sasazono@gmail.com>
2022-03-01 15:39:20 +0900

    theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
+import { CssBaseline, ThemeProvider, useMediaQuery } from "@mui/material";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { darkTheme, lightTheme } from "../core/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const isDarkScheme = useMediaQuery("(prefers-color-scheme:dark)");
+
   return (
     <RecoilRoot>
-      <Component {...pageProps} />
+      <ThemeProvider theme={isDarkScheme ? darkTheme : lightTheme}>
+        <CssBaseline />
+
+        <Component {...pageProps} />
+      </ThemeProvider>
     </RecoilRoot>
   );
 }
