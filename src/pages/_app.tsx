c59592e532a56929db98f3bbfdaa9a02e5b7df14
Cano <developer.sasazono@gmail.com>
2022-03-03 10:24:21 +0900

    add ogp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { CssBaseline, ThemeProvider, useMediaQuery } from "@mui/material";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import { darkTheme, lightTheme } from "../core/theme";
@@ -7,13 +8,28 @@ function MyApp({ Component, pageProps }: AppProps) {
   const isDarkScheme = useMediaQuery("(prefers-color-scheme:dark)");
 
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={isDarkScheme ? darkTheme : lightTheme}>
-        <CssBaseline />
+    <>
+      <DefaultSeo
+        openGraph={{
+          type: "website",
+          site_name: "Password Generator",
+          images: [
+            { url: "https://password.tepbyte.dev/icons/icon-512-maskable.png" },
+          ],
+        }}
+        twitter={{
+          cardType: "summary",
+        }}
+      />
 
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </RecoilRoot>
+      <RecoilRoot>
+        <ThemeProvider theme={isDarkScheme ? darkTheme : lightTheme}>
+          <CssBaseline />
+
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </>
   );
 }
 export default MyApp;
