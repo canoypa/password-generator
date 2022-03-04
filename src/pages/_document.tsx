505b51d691be6c43594878f7c21fd31bf9e2121b
Cano <developer.sasazono@gmail.com>
2022-03-04 10:43:14 +0900

    v0.1.1 (#4)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,64 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import { Children, FC } from "react";
+import { createEmotionCache } from "../core/emotionCache";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link rel="manifest" href="manifest.json" />
-        <link rel="icon" href="favicon.ico" sizes="any" />
-      </Head>
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage;
 
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    const cache = createEmotionCache();
+    const { extractCriticalToChunks } = createEmotionServer(cache);
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: any) => {
+          const EnhanceApp: FC = (props: any) => (
+            <App emotionCache={cache} {...props} />
+          );
+          return EnhanceApp;
+        },
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    const emotionStyles = extractCriticalToChunks(initialProps.html);
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        data-emotion={`${style.key} ${style.ids.join(" ")}`}
+        key={style.key}
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ));
+
+    return {
+      ...initialProps,
+      styles: [...Children.toArray(initialProps.styles), ...emotionStyleTags],
+    };
+  }
+
+  return() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="manifest" href="manifest.json" />
+          <link rel="icon" href="favicon.ico" sizes="any" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
