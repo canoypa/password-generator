2347d323ad86ce4883e2213aab6e61a75159e4b3
Cano <contact.sasasaky@gmail.com>
2022-03-14 16:39:51 +0900

    Theme (#7)

--- src/core/theme.ts
+++ src/core/theme.ts
@@ -1,24 +1,64 @@
-import { createTheme as createMuiTheme, PaletteOptions } from "@mui/material";
+import {
+  createTheme as createMuiTheme,
+  PaletteColor,
+  PaletteColorOptions,
+  PaletteOptions,
+} from "@mui/material";
+
+declare module "@mui/material/styles" {
+  interface PaletteOptions {
+    secondaryContainer?: PaletteColorOptions;
+    surfaceVariant?: PaletteColorOptions;
+  }
+
+  interface Palette {
+    secondaryContainer: PaletteColor;
+    surfaceVariant: PaletteColor;
+  }
+}
 
 const LightPalette: PaletteOptions = {
   mode: "light",
 
+  primary: {
+    main: "#6750A4",
+  },
+  secondaryContainer: {
+    main: "#E8DEF8",
+  },
+  surfaceVariant: {
+    main: "#E7E0EC",
+  },
+
   background: {
     default: "#FFFBFE",
+    paper: "#FFFBFE",
   },
 };
 
 const DarkPalette: PaletteOptions = {
   mode: "dark",
 
+  primary: {
+    main: "#D0BCFF",
+  },
+  secondaryContainer: {
+    main: "#4A4458",
+  },
+  surfaceVariant: {
+    main: "#49454F",
+  },
+
   background: {
     default: "#1C1B1F",
+    paper: "#1C1B1F",
   },
 };
 
 const createTheme = (palette: PaletteOptions) => {
   return createMuiTheme({
     palette: palette,
+
     components: {
       MuiListSubheader: {
         styleOverrides: {
