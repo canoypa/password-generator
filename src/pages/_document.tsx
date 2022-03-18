8d72b385ab2c7235031a9ae6a4889801dea4a3c5
Cano <developer.sasazono@gmail.com>
2022-03-18 17:39:03 +0900

    Organizing meta information (#21)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -52,6 +52,14 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="manifest.json" />
           <link rel="icon" href="favicon.ico" sizes="any" />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="Password Generator" />
+
+          <meta
+            property="og:image"
+            content="https://password.tepbyte.dev/icons/icon-512-maskable.png"
+          />
         </Head>
 
         <body>
