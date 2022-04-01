4e161b1b7cabca38d806a182c14346d5e757aa08
Cano <contact.sasasaky@gmail.com>
2022-04-01 15:54:49 +0900

    cjs transpile cnfig

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
 const withPWA = require("next-pwa");
+const withTM = require("next-transpile-modules")([
+  "@material/material-color-utilities",
+]);
 
 const { version } = require("./package.json");
 
 /** @type {import('next').NextConfig} */
-module.exports = withPWA({
-  reactStrictMode: true,
+module.exports = withTM(
+  withPWA({
+    reactStrictMode: true,
 
-  pwa: {
-    dest: "public",
-    disable: process.env.NODE_ENV === "development",
-  },
+    pwa: {
+      dest: "public",
+      disable: process.env.NODE_ENV === "development",
+    },
 
-  env: {
-    APP_VERSION: version,
-  },
-});
+    env: {
+      APP_VERSION: version,
+    },
+  })
+);
