79eeb1113540c5da73924f212d0083b40ec8577a
Cano <contact.sasasaky@gmail.com>
2022-07-01 09:03:08 +0900

    Change dialogs import to dynamically (#102)

--- src/components/settings/include_type_dialog.tsx
+++ src/components/settings/include_type_dialog.tsx
@@ -24,7 +24,7 @@ type DialogProps = {
   onClose: () => void;
   value: SettingIncludeTypes;
 };
-export const IncludeTypeDialog: FC<DialogProps> = ({
+const IncludeTypeDialog: FC<DialogProps> = ({
   open,
   onSubmit,
   onClose,
@@ -88,3 +88,4 @@ export const IncludeTypeDialog: FC<DialogProps> = ({
     </Dialog>
   );
 };
+export default IncludeTypeDialog;
