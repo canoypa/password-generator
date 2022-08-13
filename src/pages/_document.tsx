1dceca30e4c3de527a71aec4f1ca9db0f3155eee
Cano <developer.sasazono@gmail.com>
2022-08-13 13:11:46 +0900

    no use emotion cache (#117)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,51 +1,6 @@
-import createEmotionServer from "@emotion/server/create-instance";
-import Document, {
-  DocumentContext,
-  DocumentInitialProps,
-  Head,
-  Html,
-  Main,
-  NextScript,
-} from "next/document";
-import { Children, FC } from "react";
-import { createEmotionCache } from "../core/emotionCache";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const originalRenderPage = ctx.renderPage;
-
-    const cache = createEmotionCache();
-    const { extractCriticalToChunks } = createEmotionServer(cache);
-
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App: any) => {
-          const EnhanceApp: FC = (props: any) => (
-            <App emotionCache={cache} {...props} />
-          );
-          return EnhanceApp;
-        },
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-
-    const emotionStyles = extractCriticalToChunks(initialProps.html);
-    const emotionStyleTags = emotionStyles.styles.map((style) => (
-      <style
-        data-emotion={`${style.key} ${style.ids.join(" ")}`}
-        key={style.key}
-        dangerouslySetInnerHTML={{ __html: style.css }}
-      />
-    ));
-
-    return {
-      ...initialProps,
-      styles: [...Children.toArray(initialProps.styles), ...emotionStyleTags],
-    };
-  }
-
   render() {
     return (
       <Html lang="en">
