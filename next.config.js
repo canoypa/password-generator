a5cabd8843bf4ef46d0075649976addb6ea4efaa
Cano <developer.sasazono@gmail.com>
2023-02-19 22:16:58 +0900

    Bump packages (#143)

--- next.config.js
+++ next.config.js
@@ -2,19 +2,16 @@ const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV !== "production",
 });
-const withTM = require("next-transpile-modules")([
-  "@material/material-color-utilities",
-]);
 
 const { version } = require("./package.json");
 
 /** @type {import('next').NextConfig} */
-module.exports = withTM(
-  withPWA({
-    reactStrictMode: true,
+module.exports = withPWA({
+  reactStrictMode: true,
 
-    env: {
-      APP_VERSION: version,
-    },
-  })
-);
+  transpilePackages: ["@material/material-color-utilities"],
+
+  env: {
+    APP_VERSION: version,
+  },
+});
