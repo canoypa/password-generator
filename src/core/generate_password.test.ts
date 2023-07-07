272fb226495d3d2d5b690196354b669c629c4057
Cano <developer.sasazono@gmail.com>
2023-07-07 23:35:24 +0900

    update test

--- src/core/generate_password.test.ts
+++ src/core/generate_password.test.ts
@@ -1,13 +1,10 @@
 import { CharType, similarChars } from "./constant";
 import { GeneratePasswordArgs, generatePassword } from "./generate_password";
-import { getRandom, getSecureRandom } from "./get_random";
+import { getRandom } from "./get_random";
 
 // crypto を使用しているためモック
 jest.mock("./get_random");
-(getRandom as jest.Mock).mockImplementation((l) => {
-  return [...Array(l)].map(Math.random);
-});
-(getSecureRandom as jest.Mock).mockImplementation(() => {
+(getRandom as jest.Mock).mockImplementation(() => {
   return Math.random();
 });
 
@@ -30,6 +27,60 @@ describe("passwordGenerator()", () => {
     expect(/[a-z]/.test(password)).toBe(true);
     expect(/[A-Z]/.test(password)).toBe(true);
     expect(/^[a-zA-Z]/.test(password)).toBe(true);
+    expect(new RegExp(`[${similarChars}]`).test(password)).toBe(false);
+  });
+
+  test("Only digit", () => {
+    const opt: GeneratePasswordArgs = {
+      length: 8,
+      includeType: {
+        [CharType.Digit]: true,
+        [CharType.Lower]: false,
+        [CharType.Upper]: false,
+      },
+      excludeChars: similarChars,
+    };
+
+    const password = generatePassword(opt);
+
+    expect(password.length).toBe(opt.length);
+    expect(/^\d+$/.test(password)).toBe(true);
+    expect(new RegExp(`[${similarChars}]`).test(password)).toBe(false);
+  });
+
+  test("Only lower", () => {
+    const opt: GeneratePasswordArgs = {
+      length: 8,
+      includeType: {
+        [CharType.Digit]: false,
+        [CharType.Lower]: true,
+        [CharType.Upper]: false,
+      },
+      excludeChars: similarChars,
+    };
+
+    const password = generatePassword(opt);
+
+    expect(password.length).toBe(opt.length);
+    expect(/^[a-z]+$/.test(password)).toBe(true);
+    expect(new RegExp(`[${similarChars}]`).test(password)).toBe(false);
+  });
+
+  test("Only upper", () => {
+    const opt: GeneratePasswordArgs = {
+      length: 8,
+      includeType: {
+        [CharType.Digit]: false,
+        [CharType.Lower]: false,
+        [CharType.Upper]: true,
+      },
+      excludeChars: similarChars,
+    };
+
+    const password = generatePassword(opt);
+
+    expect(password.length).toBe(opt.length);
+    expect(/^[A-Z]+$/.test(password)).toBe(true);
     expect(new RegExp(`[${similarChars}]`).test(password)).toBe(false);
   });
 });
