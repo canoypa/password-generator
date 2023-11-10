cf00ebfb2325ac110d7247bc927f377883e2aa1d
Cano <developer.sasazono@gmail.com>
2023-11-10 09:23:43 +0900

    add symbols to char type

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -1,6 +1,7 @@
 export const digits = "0123456789";
 export const lowers = "abcdefghijklmnopqrstuvwxyz";
 export const uppers = lowers.toUpperCase();
+export const symbols = "*!@.-";
 
 export const similarChars = "lI1O0";
 
@@ -8,11 +9,13 @@ export const CharType = {
   Digit: "digit",
   Lower: "lower",
   Upper: "upper",
+  Symbol: "symbol",
 } as const;
-export type CharType = typeof CharType[keyof typeof CharType];
+export type CharType = (typeof CharType)[keyof typeof CharType];
 
 export const CharTypeLabel: Record<CharType, string> = {
   [CharType.Digit]: "Digit",
   [CharType.Lower]: "Lower",
   [CharType.Upper]: "Upper",
+  [CharType.Symbol]: "Symbol",
 };
