4c7d875449cc01cd2da417eb53f0d3901147d87d
Cano <developer.sasazono@gmail.com>
2023-12-09 17:47:41 +0900

    update

--- src/components/settings/password_length_field.tsx
+++ src/components/settings/password_length_field.tsx
@@ -1,99 +1,55 @@
-import {
-  FormControl,
-  FormHelperText,
-  Input,
-  ListItem,
-  ListItemText,
-  Slider,
-  Stack,
-} from "@mui/material";
+import { Input, ListItem, ListItemText, Slider, Stack } from "@mui/material";
 import { useAtom } from "jotai";
-import { ChangeEventHandler, useCallback, useState } from "react";
+import { ChangeEventHandler } from "react";
 import { passwordLengthSettingAtom } from "../../core/settings_store";
 
-type InputError =
-  | {
-      isError: true;
-      errorMessage: string;
-    }
-  | {
-      isError: false;
-      errorMessage?: never;
-    };
-
-const min = 4;
-const max = 4096;
+const LENGTH_MIN = 4;
+const LENGTH_MAX = 4096;
 
 export const PasswordLengthSettingField = () => {
   const [length, setLength] = useAtom(passwordLengthSettingAtom);
 
-  const [inputError, setInputError] = useState<InputError>({ isError: false });
+  const onChange = (newValue: number) => {
+    setLength(Math.max(Math.min(newValue, LENGTH_MAX), LENGTH_MIN));
+  };
 
-  const onChange = useCallback((newValue: number) => {
-    if (Number.isNaN(newValue))
-      setInputError({ isError: true, errorMessage: "Invalid number." });
-    else if (newValue < min)
-      setInputError({ isError: true, errorMessage: `Min value is ${min}.` });
-    else if (newValue > max)
-      setInputError({ isError: true, errorMessage: `Max value is ${max}.` });
-    else setInputError({ isError: false });
+  const onSliderChange = (_: Event, newValue: number | number[]) => {
+    if (typeof newValue !== "number") return;
+    onChange(2 ** newValue);
+  };
 
-    if (!Number.isNaN(newValue)) {
-      setLength(newValue);
-    }
-  }, []);
+  const onInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const newValue = parseInt(e.target.value, 10);
+    if (Number.isNaN(newValue)) return;
 
-  const onSliderChange = useCallback(
-    (_: Event, newValue: number | number[]) => {
-      if (typeof newValue !== "number") {
-        throw new Error("newValue is not a number");
-      }
-
-      onChange(2 ** newValue);
-    },
-    []
-  );
-
-  const onInputChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
-    (e) => {
-      const newValue = parseInt(e.target.value, 10);
-      onChange(newValue);
-    },
-    []
-  );
+    onChange(newValue);
+  };
 
   return (
     <ListItem>
       <Stack sx={{ flex: 1 }}>
         <ListItemText primary="Password Length" />
-        <Stack>
-          <Stack direction="row" spacing={2} mt={1}>
-            <Slider
-              min={2}
-              max={5}
-              size="small"
-              step={1}
-              marks
-              value={Math.log(length ?? 0) / Math.log(2)}
-              scale={(v) => 2 ** v}
-              disabled={!length}
-              onChange={onSliderChange}
-            />
-            <FormControl error={inputError.isError}>
-              <Input
-                sx={{ width: "4em" }}
-                error={inputError.isError}
-                type="number"
-                inputProps={{ min, max }}
-                value={length}
-                disabled={!length}
-                onInput={onInputChange}
-              />
-            </FormControl>
-          </Stack>
-          <FormHelperText error={inputError.isError}>
-            {inputError.errorMessage}
-          </FormHelperText>
+
+        <Stack direction="row" spacing={2} mt={1}>
+          <Slider
+            min={2}
+            max={5}
+            size="small"
+            step={1}
+            marks
+            value={Math.log(length ?? 0) / Math.log(2)}
+            scale={(v) => 2 ** v}
+            disabled={!length}
+            onChange={onSliderChange}
+          />
+          <Input
+            sx={{ width: "4em" }}
+            type="number"
+            inputProps={{ min: LENGTH_MIN, max: LENGTH_MAX }}
+            value={length ?? 0} // fix uncontrolled warning
+            disabled={!length}
+            onChange={onInputChange}
+          />
         </Stack>
       </Stack>
     </ListItem>
