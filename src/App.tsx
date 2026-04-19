22941f393327856ab5fec08c4d13e8b54cea0acf
Cano <41741040+canoypa@users.noreply.github.com>
2026-04-19 15:05:33 +0900

    chore: add vite config and entry files

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Box, Stack } from "@mui/material";
+import { Footer } from "./components/footer";
+import { Generate } from "./components/generate";
+import { Hero } from "./components/hero";
+import { Settings } from "./components/settings";
+
+export const App = () => {
+  return (
+    <Stack mx="auto" py={3} maxWidth="sm" rowGap={8}>
+      <Hero />
+
+      <Box px={3}>
+        <Generate />
+      </Box>
+
+      <Settings />
+
+      <Footer />
+    </Stack>
+  );
+};
